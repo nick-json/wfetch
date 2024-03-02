cb9968d60633b5d00efd36d9e27123dbbfccfd95
nick-json <niccoloj178@gmail.com>
2024-03-02 14:19:59 +0100

    first script

---
+++ wfetch.bat
@@ -0,0 +1,10 @@
+@echo off
+
+echo ------------------------------
+echo ^|        wFetch results      ^|
+echo ------------------------------
+echo ^| OS: %OS% 
+echo ^| Computer Name: %COMPUTERNAME%
+echo ^| User: %USERNAME%
+echo ^| Date: %DATE%
+echo ------------------------------
